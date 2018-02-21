b363cd9b6b3abaaca0124236840483b1bf02cc76
SkyMX99 - <alex.lewis@gmail.com>
2018-02-21 13:59:23 -0600

    Lab2 added

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {  View, StyleSheet } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
 
 
 
@@ -13,20 +7,8 @@ import {  View, StyleSheet } from 'react-native';
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <View style={{ flex: 1, backgroundColor: '#2ba0c9'}}></View>
-        <View style={{ flex: 1, backgroundColor: '#76b7bf'}}></View>
-        <View style={{ flex: 1, backgroundColor: '#966e56'}}></View>
-      </View>
+      <HomeScreen />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    flex: 1,
-    padding: 10,
-    backgroundColor: '#284a58',
-  }
-});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { ScrollView, Text, StyleSheet } from 'react-native';
+
+
+export default class HomeScreen extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { projects: [] };
+    }
+
+    componentDidMount() {
+        this.setState({
+            projects: [
+                { name: 'Test Project 1', description: 'Cool Project!'},
+                { name: 'Test Project 2', description: 'Cool Project!'},
+                { name: 'Test Project 3', description: 'Cool Project!'},
+                { name: 'Test Project 4', description: 'Cool Project!'},
+            ]
+        });
+    }
+    render() {
+        return(
+            <ScrollView style={styles.container}>
+                { this.state.projects.map((project, index) => {
+                    return <Text key={index}>{project.name}</Text>
+                })}
+            </ScrollView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 30,
+        paddingLeft: 10,
+        paddingRight: 10
+    }
+});
